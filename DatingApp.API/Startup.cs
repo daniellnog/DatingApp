16f91a904ddbcc099bf9a32e055d92d9bfe0216d
daniellnog <daniellnog@gmail.com>
2020-04-17 14:46:11 +0100

    Adding log in calls on Angular to API

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -52,7 +52,7 @@ namespace DatingApp.API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyMethod());
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowAnyMethod());
 
             app.UseAuthentication();
 
